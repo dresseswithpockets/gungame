2e2767da31635fe1b8b3730f3a430472e042aa3e
Ashley <dresswithpockets@pm.me>
2023-12-14 23:07:53 -0700

    move gravity to an autoload class for demonstration purposes

--- source/constants.gd
+++ source/constants.gd
@@ -5,3 +5,5 @@ const GodotUnitsPerTb: float = 1.0 / TbUnitsPerGodot
 
 const GodotToTb: float = TbUnitsPerGodot
 const TbToGodot: float = GodotUnitsPerTb
+
+var gravity: float = ProjectSettings.get_setting("physics/3d/default_gravity")
--- source/entities/player/player.gd
+++ source/entities/player/player.gd
@@ -6,9 +6,6 @@ const YAW_SPEED = 0.022
 const PITCH_SPEED = 0.022
 const SENSITIVITY = 2
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
-
 @onready var camera: Camera3D = $Camera3D
 
 func _input(event: InputEvent):
@@ -20,7 +17,7 @@ func _input(event: InputEvent):
 func _physics_process(delta):
     # Add the gravity.
     if not is_on_floor():
-        velocity.y -= gravity * delta
+        velocity.y -= Constants.gravity * delta
 
     # Handle Jump.
     if Input.is_action_just_pressed("move_jump") and is_on_floor():
