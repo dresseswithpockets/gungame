37747a5b750612f2a2b93516d323c209a081dd0d
Ashley <dresswithpockets@pm.me>
2023-12-15 14:47:46 -0700

    reformat

--- source/addons/qodot/game_definitions/worldspawn_layers/liquid/lava.gd
+++ source/addons/qodot/game_definitions/worldspawn_layers/liquid/lava.gd
@@ -2,6 +2,6 @@ class_name LavaLayer extends LiquidLayer
 
 
 func _init() -> void:
-	buoyancy_factor = 6.0
-	vertical_damping_factor = 3.0
-	lateral_damping_factor = 4.0
+    buoyancy_factor = 6.0
+    vertical_damping_factor = 3.0
+    lateral_damping_factor = 4.0
