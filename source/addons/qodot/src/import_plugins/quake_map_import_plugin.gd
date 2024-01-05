0b7377b35d1e660bbc8559cc2ebca4eda97d6ca6
Ashley <dresswithpockets@pm.me>
2024-01-05 11:36:55 -0700

    replace tabs with spaces

--- source/addons/qodot/src/import_plugins/quake_map_import_plugin.gd
+++ source/addons/qodot/src/import_plugins/quake_map_import_plugin.gd
@@ -5,42 +5,42 @@ extends EditorImportPlugin
 # Quake super.map import plugin
 
 func _get_importer_name() -> String:
-	return 'qodot.map'
+    return 'qodot.map'
 
 func _get_visible_name() -> String:
-	return 'Quake Map'
+    return 'Quake Map'
 
 func _get_resource_type() -> String:
-	return 'Resource'
+    return 'Resource'
 
 func _get_recognized_extensions() -> PackedStringArray:
-	return PackedStringArray(['map'])
-	
+    return PackedStringArray(['map'])
+    
 func _get_priority():
-	return 1.0
+    return 1.0
 
 func _get_save_extension() -> String:
-	return 'tres'
+    return 'tres'
 
 func _get_import_options(path, preset):
-	return []
+    return []
 
 func _get_preset_count() -> int:
-	return 0
-	
+    return 0
+    
 func _get_import_order():
-	return 0
+    return 0
 
 func _import(source_file, save_path, options, r_platform_variants, r_gen_files) -> Error:
-	var save_path_str = '%s.%s' % [save_path, _get_save_extension()]
+    var save_path_str = '%s.%s' % [save_path, _get_save_extension()]
 
-	var map_resource : QuakeMapFile = null
+    var map_resource : QuakeMapFile = null
 
-	var existing_resource := load(save_path_str) as QuakeMapFile
-	if(existing_resource != null):
-		map_resource = existing_resource
-		map_resource.revision += 1
-	else:
-		map_resource = QuakeMapFile.new()
+    var existing_resource := load(save_path_str) as QuakeMapFile
+    if(existing_resource != null):
+        map_resource = existing_resource
+        map_resource.revision += 1
+    else:
+        map_resource = QuakeMapFile.new()
 
-	return ResourceSaver.save(map_resource, save_path_str)
+    return ResourceSaver.save(map_resource, save_path_str)
--- source/addons/qodot/src/resources/game-definitions/trenchbroom/trenchbroom_face_attrib_defaults.gd
+++ source/addons/qodot/src/resources/game-definitions/trenchbroom/trenchbroom_face_attrib_defaults.gd
@@ -11,16 +11,16 @@ class_name TrenchBroomFaceAttribDefaults extends Resource
 @export var color: Color
 
 func _to_string() -> String:
-	var export_object: Dictionary
-	export_object["textureName"] = texture_name
-	export_object["offset"] = [offset.x, offset.y]
-	export_object["scale"] = [scale.x, scale.y]
-	export_object["rotation"] = rotation
-	if !surface_contents.is_empty():
-		export_object["surfaceContents"] = surface_contents
-	if !surface_flags.is_empty():
-		export_object["surfaceFlags"] = surface_flags
-	export_object["surfaceValue"] = surface_value
-	export_object["color"] = "%f %f %f %f" % [color.r, color.g, color.b, color.a]
+    var export_object: Dictionary
+    export_object["textureName"] = texture_name
+    export_object["offset"] = [offset.x, offset.y]
+    export_object["scale"] = [scale.x, scale.y]
+    export_object["rotation"] = rotation
+    if !surface_contents.is_empty():
+        export_object["surfaceContents"] = surface_contents
+    if !surface_flags.is_empty():
+        export_object["surfaceFlags"] = surface_flags
+    export_object["surfaceValue"] = surface_value
+    export_object["color"] = "%f %f %f %f" % [color.r, color.g, color.b, color.a]
 
-	return JSON.stringify(export_object, "\t")
+    return JSON.stringify(export_object, "\t")
