ec49701689a6553a5c1a130921e8851c292fdebe
Ashley <dresswithpockets@pm.me>
2024-01-07 16:41:06 -0700

    add some node-search functions

---
+++ source/NodeExtensions.cs
@@ -0,0 +1,32 @@
+﻿using Godot;
+
+namespace GunGame;
+
+public static class NodeExtensions
+{
+    public static T FindFirstChild<T>(this Node node) where T : Node
+    {
+        foreach (var child in node.GetChildren())
+        {
+            if (child is T t)
+                return t;
+            
+            var result = child.FindFirstChild<T>();
+            if (result != null)
+                return result;
+        }
+
+        return null;
+    }
+
+    public static T FindFirstImmediateChild<T>(this Node node) where T : Node
+    {
+        foreach (var child in node.GetChildren())
+        {
+            if (child is T t)
+                return t;
+        }
+
+        return null;
+    }
+}
