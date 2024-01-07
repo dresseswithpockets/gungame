2343149a3d15cd4a92e4f88e32a74c1475320a15
Ashley <dresswithpockets@pm.me>
2024-01-07 00:48:57 -0700

    add aditional GetOrDefault overloads

--- source/GodotCollectionsExtensions.cs
+++ source/GodotCollectionsExtensions.cs
@@ -1,9 +1,18 @@
+using System;
+using System.Runtime.CompilerServices;
+using Godot;
 using Godot.Collections;
 
 namespace GunGame;
 
 public static class GodotCollectionsExtensions
 {
-    public static int GetOrDefault(this Dictionary dictionary, string key, int @default)
-        => dictionary.TryGetValue(key, out var value) ? value.AsInt32() : default;
+    public static int GetOrDefault(this Dictionary dictionary, string key, int @default = default)
+        => dictionary.TryGetValue(key, out var value) ? value.AsInt32() : @default;
+    
+    public static bool GetOrDefault(this Dictionary dictionary, string key, bool @default = default)
+        => dictionary.TryGetValue(key, out var value) ? value.AsBool() : @default;
+    
+    public static Vector3 GetOrDefault(this Dictionary dictionary, string key, Vector3 @default = default)
+        => dictionary.TryGetValue(key, out var value) ? value.AsVector3() : @default;
 }
