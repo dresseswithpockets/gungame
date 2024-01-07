e3125c3bbb6fad63e103d1c5ad9b1cbe64847c54
Ashley <dresswithpockets@pm.me>
2024-01-07 16:40:51 -0700

    add extension method for applying an axis mask to a vector

--- source/AxisMask.cs
+++ source/AxisMask.cs
@@ -8,3 +8,18 @@ public enum AxisMask
     Y = 1 << 1,
     Z = 1 << 2,
 }
+
+public static class AxisMaskExtensions
+{
+    public static void ApplyMaskedVector(this AxisMask mask, ref Vector3 target, ref Vector3 amount)
+    {
+        if (mask.HasFlag(AxisMask.X))
+            target.X = amount.X;
+
+        if (mask.HasFlag(AxisMask.Y))
+            target.Y = amount.Y;
+
+        if (mask.HasFlag(AxisMask.Z))
+            target.Z = amount.Z;
+    }
+}
