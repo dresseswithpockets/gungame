80eed566ae50d6110b3fed7528bab7b5b6ee37dc
Ashley <dresswithpockets@pm.me>
2024-01-07 17:09:46 -0700

    add GetOrDefault override for float

--- source/GodotCollectionsExtensions.cs
+++ source/GodotCollectionsExtensions.cs
@@ -10,6 +10,9 @@ public static class GodotCollectionsExtensions
     public static int GetOrDefault(this Dictionary dictionary, string key, int @default = default)
         => dictionary.TryGetValue(key, out var value) ? value.AsInt32() : @default;
     
+    public static float GetOrDefault(this Dictionary dictionary, string key, float @default = default)
+        => dictionary.TryGetValue(key, out var value) ? value.AsSingle() : @default;
+    
     public static bool GetOrDefault(this Dictionary dictionary, string key, bool @default = default)
         => dictionary.TryGetValue(key, out var value) ? value.AsBool() : @default;
     
