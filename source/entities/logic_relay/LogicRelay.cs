4c93556edcbbf4910ea1757a73cc830a4c2d108f
Ashley <dresswithpockets@pm.me>
2024-01-07 22:51:20 -0700

    add logic_relay

---
+++ source/entities/logic_relay/LogicRelay.cs
@@ -0,0 +1,43 @@
+using Godot;
+using GunGame;
+
+[Tool]
+public partial class LogicRelay : QodotBaseEntity
+{
+    [Signal]
+    // ReSharper disable once InconsistentNaming
+    public delegate void triggerEventHandler(Node3D activator);
+
+    [Export] public bool startDisabled;
+    [Export] public bool onlyOnce;
+
+    private bool _enabled;
+    private bool _fired;
+
+    public override void _Ready() => _enabled = !startDisabled;
+    
+    protected override void UpdateProperties()
+    {
+        if (!Engine.IsEditorHint())
+            return;
+
+        startDisabled = properties.GetOrDefault("start_disabled", false);
+        onlyOnce = properties.GetOrDefault("only_once", false);
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public void use(Node3D activator)
+    {
+        if (!_enabled) return;
+        if (onlyOnce && _fired) return;
+        
+        EmitSignal(SignalName.trigger, activator);
+        _fired = true;
+    }
+
+    // ReSharper disable once InconsistentNaming
+    public void enable(Node3D _) => _enabled = true;
+
+    // ReSharper disable once InconsistentNaming
+    public void disable(Node3D _) => _enabled = false;
+}
