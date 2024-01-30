0541f2651249f6fe797b0b15d77966bbb1c91ee8
Ashley <dresswithpockets@pm.me>
2024-01-29 21:53:16 -0700

    add a basic player inventory autoload

---
+++ source/PlayerInventory.cs
@@ -0,0 +1,17 @@
+﻿using Godot;
+
+namespace GunGame;
+
+public partial class PlayerInventory : Node
+{
+    public readonly bool[] keys = new bool[4];
+    
+    [Signal]
+    public delegate void KeyAddedEventHandler(ItemKeyColor color);
+    
+    public void AddKey(ItemKeyColor color)
+    {
+        keys[(int)color] = true;
+        EmitSignal(SignalName.KeyAdded, (int)color);
+    }
+}
