6e78acd947c06354c71d302c1e9aa2d3290e4e9c
Ashley <dresswithpockets@pm.me>
2024-02-01 16:51:33 -0700

    add qodot map utility extensions

--- source/NodeExtensions.cs
+++ source/NodeExtensions.cs
@@ -1,4 +1,6 @@
-﻿using Godot;
+﻿using System;
+using Godot;
+using Godot.Collections;
 
 namespace GunGame;
 
@@ -28,5 +30,58 @@ public static class NodeExtensions
         }
 
         return null;
+    }
+    
+    public static bool QodotMapOneOrNull<T>(this GodotObject qodotMap, string sourceName, string targetName, out T entity) where T : Node
+    {
+        var nodes = qodotMap.Call("get_nodes_by_targetname", targetName).AsGodotObjectArray<Node>();
+        switch (nodes.Length)
+        {
+            case 0:
+                GD.PushWarning(
+                    $"'{sourceName}' targets '{targetName}', but there are no entities with that name, so it will not link to anything.");
+                entity = null;
+                return false;
+            case > 1:
+                GD.PushWarning($"'{sourceName}' targets multiple entities named '{targetName}'. Will only link to the first one.");
+                break;
+        }
+
+        var one = nodes[0];
+        if (one is not T t)
+        {
+            GD.PushWarning($"'{sourceName}' expects a {typeof(T).Name}-derived type, but '{targetName}' doesn't derive from {typeof(T).Name}.");
+            entity = null;
+            return false;
+        }
+
+        entity = t;
+        return true;
+    }
+
+    public static void QodotMapGetAllTargets<[MustBeVariant] T>(
+        this GodotObject qodotMap,
+        string sourceName,
+        Dictionary properties,
+        ref Array<T> array)
+        where T : Node
+    {
+        if (array == null)
+            array = new Array<T>();
+        else
+            array.Clear();
+        
+        var target = properties.GetOrDefault("target", "");
+        if (!string.IsNullOrWhiteSpace(target) && QodotMapOneOrNull(qodotMap, sourceName, target, out T t))
+            array.Add(t);
+
+        foreach (var (keyVariant, value) in properties)
+        {
+            var key = keyVariant.AsString();
+            if (!key.StartsWith("target") || key == "target") continue;
+            if (!int.TryParse(key.AsSpan(6), out _)) continue;
+            if (QodotMapOneOrNull(qodotMap, sourceName, value.AsString(), out t))
+                array.Add(t);
+        }
     }
 }
