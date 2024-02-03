bbc89fe4a5b04e71c4a42cb3ba12f7ea142f0f36
Ashley <dresswithpockets@pm.me>
2024-02-03 11:07:14 -0700

    move QodotMath to its own file

---
+++ source/QodotMath.cs
@@ -0,0 +1,14 @@
+﻿using Godot;
+
+public static class QodotMath
+{
+    public static Vector3 TbAnglesToDirection(Vector3 angles)
+    {
+        angles.X = Mathf.DegToRad(-angles.X);
+        angles.Y = Mathf.DegToRad(angles.Y + 180);
+        angles.Z = Mathf.DegToRad(angles.Z);
+        return Basis.FromEuler(angles) * Vector3.Forward;
+    }
+
+    public static float TbUnitsToGodot(float amount) => amount / 128f;
+}
